488941d65d282c34bf22a7f7641225c3100ab067
KoulteghDevOps <koultegh@outlook.com>
2023-05-29 19:15:05 +0100

    manifest updated

--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
     all_private_subnet_ids = concat(module.subnets["webserver"].route_table_ids,module.subnets["application"].route_table_ids,module.subnets["database"].route_table_ids)
+    all_route_table_ids = concat(module.subnets["webserver"].route_table_ids,module.subnets["application"].route_table_ids,module.subnets["database"].route_table_ids)
 }
